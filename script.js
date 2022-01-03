92122e7cafe2d04f3e1fd08118573ba85aecc531
JobdV <jobdvogel@gmail.com>
2022-01-03 20:13:20 +0100

    Added animation to timeline

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // DEVELOPED BY JOB DE VOGEL
-
 // Hide navbar background
 function changeBackground(){
     if(window.scrollY > window.innerHeight / 3){
@@ -10,6 +9,28 @@ function changeBackground(){
 }
 
 window.addEventListener('scroll', changeBackground);
+
+const section = document.querySelector("#_2019");
+
+const options = {
+    root: null,
+    treshold: 0.9,
+    rootMargin: "0px"
+
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            document.querySelector(".timeline-title").style.animation = "anim_timeline ease-in 8s forwards";
+            observer.unobserve(entry.target);
+            return
+        }
+        
+    });
+}, options);
+
+observer.observe(section);
 
 // Manual carousel credits @w3school
 var slideIndex = 1;
@@ -51,7 +72,7 @@ function addInformation(input, result, visibleStyle, invisibleStyle){
     // Question and answer for software/skills information
     const questions = document.getElementById(input);
     const answers = document.getElementById(result);
-    const widthFactor = 1.5;
+    const widthFactor = .5;
 
     var infoVisible = false;
     var tempInfoVisible= false;
@@ -70,9 +91,9 @@ function addInformation(input, result, visibleStyle, invisibleStyle){
         questions.children[i].addEventListener("mouseover", function(){
             if (infoVisible == false){
                 const pLength = answers.children[i].innerText.length;
-                const factor = 83 + (Math.max(pLength - 791, 0)) * widthFactor;
+                const factor = 208 + (Math.max(pLength - 519, 0)) * widthFactor;
 
-                answers.children[i].style.width = factor + '%';
+                answers.children[i].style.width = factor + 'px';
                 answers.children[i].classList.remove(invisibleStyle);
                 answers.children[i].classList.add(visibleStyle);
 
@@ -113,12 +134,14 @@ function addInformation(input, result, visibleStyle, invisibleStyle){
                 questions.children[i].style.transition = 'font-weight .2s';
                 questions.children[i].style.fontWeight = '500';
 
+                
                 const pLength = answers.children[i].innerText.length;
-                const factor = 83 + (Math.max(pLength - 791, 0)) * widthFactor;
+                const factor = 208 + (Math.max(pLength - 519, 0)) * widthFactor;
 
-                answers.children[i].style.width = factor + '%';
+                answers.children[i].style.width = factor + 'px';
                 answers.children[i].classList.remove(invisibleStyle);
                 answers.children[i].classList.add(visibleStyle);
+                
             }
 
             infoVisible = true;
@@ -131,8 +154,9 @@ function addInformation(input, result, visibleStyle, invisibleStyle){
         document.addEventListener("click", function(event){
             if (infoVisible == true){
                 var isClickOnQuestion = visibleQuestion.contains(event.target);
+                var isClickOnAnswer = infoScreen.contains(event.target);
                 
-                if (!isClickOnQuestion){
+                if (!isClickOnQuestion && !isClickOnAnswer){
                     infoScreen.classList.remove(visibleStyle);
                     infoScreen.classList.add(invisibleStyle);
                     visibleQuestion.style.transition = 'font-weight .2s';
@@ -145,4 +169,80 @@ function addInformation(input, result, visibleStyle, invisibleStyle){
     }
 }
 
+/*
+// Question and answer for software/skills information
+const questions = document.getElementById('questions_test');
+const answers_left = document.getElementById('answers_test_left');
+const answers_right = document.getElementById('answers_test_right')
+const widthFactor = .5;
+
+var infoVisible = false;
+var tempInfoVisible= false;
+var visibleQuestion;
+var infoScreen;
+
+
+
+// If scroll is at page skills...
+    // Example selection    
+
+    // Manual selection
+//
+
+// Make a function, one for left bar, with questions input answers input
+for(let i = 0; i < questions.children.length; i++){   
+    let j;
+    let answers;
+
+    // If user hovers over software type, show the explanation
+    if (i == 0){
+        answers = answers_left;
+        j = 0
+    } else if (i == 1){
+        answers = answers_right;
+        j = 0
+    } else if (i == 2){
+        answers = answers_left;
+        j = 1
+    } else if (i == 3){
+        answers = answers_right;
+        j = 1
+    }
+ 
+    questions.children[i].addEventListener("mouseover", function(){
+        if (infoVisible == false){
+            
+            const pLength = answers.children[j].innerText.length;
+            const factor = 208 + (Math.max(pLength - 519, 0)) * widthFactor;
+
+            answers.children[j].style.width = factor + 'px';
+            
+            answers.children[j].classList.remove('invisible_test');
+            answers.children[j].classList.add('visible_test');
+
+            questions.children[i].style.transition = 'font-weight .2s';
+            questions.children[i].style.fontWeight = '500';
+            questions.children[i].style.textDecoration = 'none';
+
+            tempInfoVisible = true;
+        }      
+    })
+    
+    // If user moves out software type, hide the explanation
+    questions.children[i].addEventListener("mouseout", function(){
+        if (infoVisible == false && tempInfoVisible == true){
+            
+            answers.children[j].classList.remove('visible_test');
+            answers.children[j].classList.add('invisible_test');
+            
+            questions.children[i].style.transition = 'font-weight .2s';
+            questions.children[i].style.fontWeight = '100';
+
+            tempInfoVisible = false;
+        }      
+    })   
+}
+
+
 // Execute function here, once for experienced and once for starter
+*/
